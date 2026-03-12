c3700fea27fcaef88a532f4b86154e5f335e9dc1
Marek Suchánek <marek.suchanek@canonical.com>
2026-03-12 14:23:24 +0100

    Moving to docs.ubuntu.com

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://ubuntu-release-notes.readthedocs-hosted.com/"
+ogp_site_url = "https://documentation.ubuntu.com/release-notes/"
 
 
 # Preview name of the documentation website
@@ -153,7 +153,7 @@ html_theme_options = {
 # TODO: If your documentation is hosted on https://docs.ubuntu.com/,
 #       uncomment and update as needed.
 
-# slug = ''
+slug = 'release-notes'
 
 #######################
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
