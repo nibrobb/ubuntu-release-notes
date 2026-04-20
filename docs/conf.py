d34d84a9329ca872b314d1558fcf1c8ff279ad0b
Marek Suchánek <marek.suchanek@canonical.com>
2026-04-20 14:35:22 +0200

    Escape regexes

--- docs/conf.py
+++ docs/conf.py
@@ -262,13 +262,13 @@ linkcheck_ignore = [
     r"https://discourse\.ubuntu\.com/t/edubuntu-.*",
     r"https://discourse\.ubuntu\.com/t/ubuntu-studio-.*",
     # Various, probably rate-limited
-    r"https://thekelleys.org.uk/gitweb/.*",
-    r"https://git.openldap.org/.*",
-    r"https://github.com/canonical/.*",
-    r"https://github.com/snapcore/.*",
-    r"https://github.com/systemd/.*",
-    r"https://ubuntustudio.org/ubuntu-studio-.*-release-notes/",
-    r"https://www.monitoring-plugins.org/news/.*",
+    r"https://thekelleys\.org\.uk/gitweb/.*",
+    r"https://git\.openldap\.org/.*",
+    r"https://github\.com/canonical/.*",
+    r"https://github\.com/snapcore/.*",
+    r"https://github\.com/systemd/.*",
+    r"https://ubuntustudio\.org/ubuntu-studio-.*-release-notes/",
+    r"https://www.monitoring-plugins\.org/news/.*",
 ]
 
 
